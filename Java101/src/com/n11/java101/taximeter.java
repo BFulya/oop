d685a4b349c906446bc4bcc3216b3db627d32705
BasakFulya <basakfulyaytu@gmail.com>
2026-05-04 17:50:40 +0300

    Update

--- Java101/src/com/n11/java101/taximeter.java
+++ Java101/src/com/n11/java101/taximeter.java
@@ -6,20 +6,16 @@ public class taximeter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double km;
         double perKm = 2.20;
         double startFee = 10;
-        double total;
 
         System.out.print("Enter the distance in kilometers: ");
-        km = input.nextDouble();
+        double km = input.nextDouble();
 
-        total = startFee + (km * perKm);
+        double total = startFee + (km * perKm);
 
-        // Minimum fare check
-        if (total < 20) {
-            total = 20;
-        }
+        // Ternary operator
+        total = (total < 20) ? 20 : total;
 
         System.out.println("Total Taximeter Fare: " + total + " TL");
     }
