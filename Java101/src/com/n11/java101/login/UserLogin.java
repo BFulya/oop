1fb6797ddfd5775cd5e5163847dbb9df75c0f993
BasakFulya <basakfulyaytu@gmail.com>
2026-05-05 01:26:56 +0300

    User Login

---
+++ Java101/src/com/n11/java101/login/UserLogin.java
@@ -0,0 +1,44 @@
+package com.n11.java101.login;
+
+import java.util.Scanner;
+
+public class UserLogin {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String correctPassword = "java123"; // mevcut şifre
+
+        System.out.print("Enter your password: ");
+        String password = input.nextLine();
+
+        if (password.equals(correctPassword)) {
+            System.out.println("Login successful");
+        } 
+        else {
+            System.out.println("Wrong password!");
+            System.out.print("Do you want to reset your password? (yes/no): ");
+            String choice = input.nextLine();
+
+            if (choice.equalsIgnoreCase("yes")) {
+
+                System.out.print("Enter new password: ");
+                String newPassword = input.nextLine();
+
+                if (newPassword.equals(correctPassword)) {
+                    System.out.println("Password could not be created, please choose a different password.");
+                } 
+                else {
+                    System.out.println("Password created successfully");
+                    correctPassword = newPassword;
+                }
+
+            } 
+            else {
+                System.out.println("Password reset cancelled.");
+            }
+        }
+
+        input.close();
+    }
+}
