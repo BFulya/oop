68b3d991d49ab30e32b0407f1e0a4d8e4872c643
BasakFulya <basakfulyaytu@gmail.com>
2026-06-13 20:48:32 +0300

    Create palindromeNumber.java
    Palindrome Number

---
+++ Java101/src/com/n11/java101/bmı/palindromeNumber.java
@@ -0,0 +1,25 @@
+public class palindromeNumber {
+
+    static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+    public static void main(String[] args) {
+        int number = 9889;
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome number.");
+        } else {
+            System.out.println(number + " is not a palindrome number.");
+        }
+    }
+}
