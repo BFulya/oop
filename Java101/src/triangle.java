34930b164cb8e1e85f75584f6ac5095f28485ee5
BasakFulya <basakfulyaytu@gmail.com>
2026-06-13 20:07:04 +0300

    Create triangle.java
    Triangle

---
+++ Java101/src/triangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class triangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digit number: ");
+        int n = input.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+            System.out.println();
+        }
+    }
